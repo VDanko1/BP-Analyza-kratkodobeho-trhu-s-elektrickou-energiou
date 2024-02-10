81b2ca0db91e42aa1f5aebc6bf7674269b50e5ae
VDanko1 <vdanko923@gmail.com>
2024-02-10 14:45:03 +0100

    add all things

--- priceGraphsDaily.py
+++ priceGraphsDaily.py
@@ -5,7 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-def visualize_av_price_in_week(filename):
+
+def visualize_av_price_in_day(filename):
     try:
         # Load the pickle file
         with open(filename, "rb") as file:
@@ -15,25 +16,20 @@ def visualize_av_price_in_week(filename):
         df = pd.DataFrame(data)
 
         # Convert 'deliveryDay' and 'deliveryStart' columns to datetime type
-        df['deliveryDay'] = pd.to_datetime(df['deliveryDay'])
-        df['deliveryStart'] = pd.to_datetime(df['deliveryStart'])
+        prices = [entry['priceWeightedAverage'] for entry in data]
 
         # Set 'deliveryStart' as the index for time series plotting
         df.set_index('deliveryStart', inplace=True)
 
         # Create the plot
         plt.figure(figsize=(20, 6))
-        plt.plot(df.index, df['priceWeightedAverage'], linestyle='-', marker="*", color='b')
+        plt.plot(prices, linestyle='-', marker=".", color='b', label='5.12.2023')
 
         plt.title('Price Trend from 4.12.2023 up to 11.12.2023')
         plt.xlabel('Date')
         plt.ylabel('Average price €/MWh')
         plt.xticks(rotation=45)
         plt.tight_layout()
-
-        # Add legend with date and day of the week
-        legend_labels = [f"{date.date()} - {date.strftime('%A')}" for date in df.index]
-        plt.legend(legend_labels, bbox_to_anchor=(1.05, 1), loc='upper left', fontsize=8)
 
         # Save the plot
         plt.savefig("continuous_prices.png")
@@ -46,7 +42,8 @@ def visualize_av_price_in_week(filename):
     except Exception as e:
         print(f"Error: {e}")
 
-def visualize_av_price_in_week_overlay(filename, filename1, filename2, filename3):
+
+def visualize_av_price_in_day_overlay(filename, filename1, filename2, filename3, filename4, filename5,filename6):
     try:
         # Load the pickle files
         with open(filename, "rb") as file_2022:
@@ -61,6 +58,16 @@ def visualize_av_price_in_week_overlay(filename, filename1, filename2, filename3
         with open(filename3, "rb") as file_2023:
             data3 = pickle.load(file_2023)
 
+        with open(filename4, "rb") as file_2023:
+            data4 = pickle.load(file_2023)
+
+        with open(filename5, "rb") as file_2023:
+            data5 = pickle.load(file_2023)
+
+        with open(filename6, "rb") as file_2023:
+            data6 = pickle.load(file_2023)
+
+
         # Extract relevant information for plotting
         dates = [entry['deliveryDay'] for entry in data]
         prices = [entry['priceWeightedAverage'] for entry in data]
@@ -73,22 +80,30 @@ def visualize_av_price_in_week_overlay(filename, filename1, filename2, filename3
 
         dates_3 = [entry['deliveryDay'] for entry in data3]
         prices_3 = [entry['priceWeightedAverage'] for entry in data3]
+        prices_4 = [entry['priceWeightedAverage'] for entry in data4]
+        prices_5 = [entry['priceWeightedAverage'] for entry in data5]
+        prices_6 = [entry['priceWeightedAverage'] for entry in data6]
+
 
         # Create the plot
         plt.figure(figsize=(20, 6))
-        plt.plot(prices, linestyle='-', marker=".", color='b', label='Week 4.12 up to 10.12')
-        plt.plot(prices_1, linestyle='-', marker=".", color='r', label='Week 11.12 up to 17.12')
-        plt.plot(prices_2, linestyle='-', marker=".", color='g', label='Week 18.12 up to 24.12')
-        plt.plot(prices_3, linestyle='-', marker=".", color='y', label='Week 25.12 up to 31.12')
+        plt.plot(prices, linestyle='-', marker=".", color='b', label='5.12.2023')
+        plt.plot(prices_1, linestyle='-', marker=".", color='r', label='6.12.2023')
+        plt.plot(prices_2, linestyle='-', marker=".", color='g', label='7.12.2023')
+        plt.plot(prices_3, linestyle='-', marker=".", color='y', label='8.12.2023')
+        plt.plot(prices_4, linestyle='-', marker=".", color='c', label='9.12.2023')
+        plt.plot(prices_5, linestyle='-', marker=".", color='#FF5733', label='10.12.2023')
+        plt.plot(prices_6, linestyle='-', marker=".", color='purple', label='11.12.2023')
 
-        days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-        plt.title('Weekly price trends between 4.12 up to 31.12')
-        plt.xlabel('Date')
-        plt.ylabel('Average price €/MWh')
+        #days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        plt.title('Daily price trends from 5.12.2023 to 11.12.2023')
+        plt.xlabel('Period (hour in day)')
+        plt.ylabel('Price €/MWh')
         plt.xticks()
         plt.legend()
+        plt.grid(alpha=0.3, zorder=-1)
         plt.tight_layout()
-
+        plt.savefig("Graphs/Tyzdnovy graf overlay.png")
         # Show the plot
         plt.show()
 
@@ -97,17 +112,11 @@ def visualize_av_price_in_week_overlay(filename, filename1, filename2, filename3
     except Exception as e:
         print(f"Error: {e}")
 
-# Example usage with four filenames
-visualize_av_price_in_week_overlay(
-    "Data/IDM_results_2023_04_12-10_12.pkl",
-    "Data/IDM_results_2023_11_12-17_12.pkl",
-    "Data/IDM_results_2023_18_12-24_12.pkl",
-    "Data/IDM_results_2023_25_12-31_12.pkl"
-)
 
-file_list = [
-    "IDM_results_2023_04_12-10_12.pkl",
-    "IDM_results_2023_11_12-17_12.pkl",
-    "IDM_results_2023_18_12-24_12.pkl",
-    "IDM_results_2023_25_12-31_12.pkl"
-]
+visualize_av_price_in_day_overlay("Data/IDM_results_2023-12-06.pkl",
+                                  "Data/IDM_results_2023-12-05.pkl"
+                                  , "Data/IDM_results_2023-12-07.pkl"
+                                  , "Data/IDM_results_2023-12-08.pkl"
+                                  , "Data/IDM_results_2023-12-09.pkl"
+                                  , "Data/IDM_results_2023-12-10.pkl"
+                                  , "Data/IDM_results_2023-12-11.pkl")
--- priceGraphsYearly.py
+++ priceGraphsYearly.py
@@ -6,18 +6,52 @@ from datetime import datetime
 import numpy as np
 
 
-
-def load_and_store_data():
-    api_url = "https://isot.okte.sk/api/v1/idm/results?deliveryDayFrom=2023-01-01&deliveryDayTo=2023-12-31&productType=60"
+def load_and_store_data_okte():
+    api_url = "https://isot.okte.sk/api/v1/idm/results?deliveryDayFrom=2023-12-11&deliveryDayTo=2023-12-11&productType=60"
 
     response = requests.get(api_url)
 
     if response.status_code == 200:
-        filename = "Data/IDM_results_2023_correct.pkl"
+        filename = "Data/IDM_results_2023-12-11.pkl"
         with open(filename, "wb") as file:
             pickle.dump(response.json(), file)
     else:
         print(f"Error: {response.status_code}")
+
+
+def load_and_store_data_oil():
+    from_date_str = "2023-01-01"
+    to_date_str = "2023-12-31"
+
+    api_url = f"https://api.oilpriceapi.com/v1/prices?past_week"#[from]={from_date_str}&by_period[to]={to_date_str}"
+
+    headers = {
+        'Authorization': 'Token c22a61b902e22643052cb26ad2b7c413',
+        'Content-Type': 'application/json'
+    }
+
+
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        filename = "Data/Oil_price_past_year"
+        with open(filename, "wb") as file:
+            pickle.dump(response.json(), file)
+            print(response.status_code)
+
+    else:
+        print(f"Error: {response.status_code}")
+
+def print_data ():
+    import pickle
+
+    # Load the pickled data
+    filename = "Data/Oil_price_2021"
+    with open(filename, "rb") as file:
+        oil_price_data = pickle.load(file)
+
+    # Now you can work with the 'oil_price_data' variable, which contains the loaded data
+    print(oil_price_data)
 
 
 def visualize_av_price_in_year(filename):
@@ -131,31 +165,31 @@ def visualize_av_prices_not_overlay(filename_2022, filename_2023):
 
 from collections import defaultdict
 
+
 def visualize_av_prices_overlay(filename_2022, filename_2023):
     try:
         # Load the pickle files
         with open(filename_2022, "rb") as file_2022:
-            data_2022 = pickle.load(file_2022)
+            data = pickle.load(file_2022)
 
         with open(filename_2023, "rb") as file_2023:
-            data_2023 = pickle.load(file_2023)
+            data_1 = pickle.load(file_2023)
 
         # Extract relevant information for plotting
-        dates_2022 = [entry['deliveryDay'] for entry in data_2022]
-        prices_2022 = [entry['priceWeightedAverage'] for entry in data_2022]
+        dates_2022 = [entry['deliveryDay'] for entry in data]
+        price = [entry['priceWeightedAverage'] for entry in data]
 
-        dates_2023 = [entry['deliveryDay'] for entry in data_2023]
-        prices_2023 = [entry['priceWeightedAverage'] for entry in data_2023]
-
+        dates_2023 = [entry['deliveryDay'] for entry in data_1]
+        price1 = [entry['price'] for entry in data_1]
 
         # Create the plot with two lines
         plt.figure(figsize=(20, 6))
-        plt.plot(prices_2022, linestyle='-', color='g', label='Prices 2022')
-        plt.plot(prices_2023, linestyle='-', color='r', label='Prices 2023')
+        plt.plot(price, linestyle='-', color='g', label='Prices 2022')
+        plt.plot(price1, linestyle='-', color='r', label='Prices 2023')
         plt.title('Price Trend from API Data')
         plt.xlabel('Months in a year')
         plt.ylabel('Average price €/MWh')
-        plt.xticks(np.arange(0, len(prices_2022)+1, len(prices_2022)//12), range(0, 13))
+        plt.xticks(np.arange(0, len(price) + 1, len(price) // 12), range(0, 13))
         plt.tight_layout()
 
         # Show the plot
@@ -166,10 +200,11 @@ def visualize_av_prices_overlay(filename_2022, filename_2023):
         print(f"Error: {e}")
 
 
-#visualize_av_prices_two_years("IDM_results_2020.pkl", "IDM_results_2021.pkl")
-visualize_av_prices_overlay("Data/IDM_results_2022.pkl", "Data/IDM_results_2023_correct.pkl")
-#load_and_store_data()
-#visualize_av_prices_overlay("IDM_results_2022.pkl", "IDM_results_2023.pkl")
-#visualize_av_prices_not_overlay("IDM_results_2022.pkl", "IDM_results_2023.pkl")
-#visualize_av_price_in_year("IDM_results_2020.pkl")
-
+# visualize_av_prices_two_years("IDM_results_2020.pkl", "IDM_results_2021.pkl")
+#visualize_av_prices_overlay("Data/IDM_results_2021.pkl", "Data/Oil_price_2021")
+load_and_store_data_okte()
+#load_and_store_data_oil()
+#print_data()
+# visualize_av_prices_overlay("IDM_results_2022.pkl", "IDM_results_2023.pkl")
+# visualize_av_prices_not_overlay("IDM_results_2022.pkl", "IDM_results_2023.pkl")
+# visualize_av_price_in_year("IDM_results_2020.pkl")
